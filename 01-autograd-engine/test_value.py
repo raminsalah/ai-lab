d11065689ec01b269c1877ce9cc950ff5872de76
Ramin Salah <raminsalahshoor@gmail.com>
2026-06-20 09:40:44 +0200

    Add initial Value class

---
+++ 01-autograd-engine/test_value.py
@@ -0,0 +1,7 @@
+from value import Value
+
+a = Value(2.0)
+b = Value(-3.0)
+
+print(a)
+print(b)
---
+++ 01-autograd-engine/value.py
@@ -0,0 +1,6 @@
+class Value:
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
