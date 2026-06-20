526b22e2a9677ca0134b212ebe22bf0abf9d5006
Ramin Salah <raminsalahshoor@gmail.com>
2026-06-20 12:35:41 +0200

    automatic backward

--- 01-autograd-engine/test_value.py
+++ 01-autograd-engine/test_value.py
@@ -1,7 +1,21 @@
 from value import Value
 
 a = Value(2.0)
-b = Value(-3.0)
+b = Value(3.0)
 
-print(a)
-print(b)
+c = a * b
+d = c + a
+
+print("forward:")
+print("a =", a)
+print("b =", b)
+print("c = a * b =", c)
+print("d = c + a =", d)
+
+d.backward()
+
+print("\nafter backward:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+print("d =", d)
--- 01-autograd-engine/value.py
+++ 01-autograd-engine/value.py
@@ -1,6 +1,47 @@
 class Value:
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=""):
         self.data = data
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._op = _op
+        self._backward = lambda: None
 
     def __repr__(self):
-        return f"Value(data={self.data})"
+        return f"Value(data={self.data}, grad={self.grad})"
+
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), "+")
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+        for v in reversed(topo):
+            v._backward()
